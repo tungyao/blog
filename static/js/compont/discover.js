d0cf943b72f1ff36793ff487216a283f5e5038d9
tung <1922527051@qq.com>
2019-09-25 23:14:11 +0800

    success:wirte style and html

--- static/js/compont/discover.js
+++ static/js/compont/discover.js
@@ -1,34 +1,39 @@
 //TODO 显示主页
 let discover = new Mosaic({
     name: "discover-main",
-    data:{switchPage:null},
+    data: {switchPage: null},
+    data:{
+        list:["this is a list title and list body1","this is a list title and list body2","this is a list title and list body3","this is a list title and list body4"],
+        color:3
+    },
     view() {
         return html`
         <div>
-        <ul>
-        <li></li>
+        <ul class="discover-list">
+        ${this.data.list.map((k,v)=>html`<li class="discover-list-item color${parseInt(Math.random()*5+1)}">${this.data.list[v]}</li>`)}
+        
 </ul>
 </div>
         `;
     }
 });
 new Mosaic({
-    name:"my-discover",
-    element:"discover",
-    data:{pointer:"discover"},
-    switchPage(){
-        this.data.pointer =this.data.pointer!=='other'?'other':'discover';
+    name: "my-discover",
+    element: "discover",
+    data: {pointer: "discover"},
+    switchPage() {
+        this.data.pointer = this.data.pointer !== 'other' ? 'other' : 'discover';
     },
-    selectIndex(){
-        if (this.data.pointer==="discover") {
-                return html`<discover-main down="${this.switchPage.bind(this)}"></discover-main>`
-        }else{
+    selectIndex() {
+        if (this.data.pointer === "discover") {
+            return html`<discover-main down="${this.switchPage.bind(this)}"></discover-main>`
+        } else {
             return html`<h1>OTHER </h1>`
         }
     },
-    view(){
+    view() {
         return html`
-        <div style="height: ${window.innerHeight}px;">
+        <div style="height: ${window.innerHeight}px;width: 1120px;">
         ${this.selectIndex()}
 </div> 
         `;
